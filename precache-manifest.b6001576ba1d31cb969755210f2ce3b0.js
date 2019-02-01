6ef481e7eb6ddd0bc18a66feff28261752de9c34
Roxana <roxana.stelea01@gmail.com>
2019-02-01 22:28:52 +0200

    Updates

---
+++ precache-manifest.b6001576ba1d31cb969755210f2ce3b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7999c328224681108cb11bdc71e08d1f",
+    "url": "/React-Weather-app/static/media/weather.7999c328.jpg"
+  },
+  {
+    "revision": "2e4be4a2b7ef9ac286df769d5bde5a77",
+    "url": "/React-Weather-app/static/media/search.2e4be4a2.svg"
+  },
+  {
+    "revision": "e96f15d46c0223666f30",
+    "url": "/React-Weather-app/static/js/runtime~main.e96f15d4.js"
+  },
+  {
+    "revision": "de74660ae55a4b042e37",
+    "url": "/React-Weather-app/static/js/main.de74660a.chunk.js"
+  },
+  {
+    "revision": "5986d0d841c9a4f625d7",
+    "url": "/React-Weather-app/static/js/1.5986d0d8.chunk.js"
+  },
+  {
+    "revision": "de74660ae55a4b042e37",
+    "url": "/React-Weather-app/static/css/main.cb5e8298.chunk.css"
+  },
+  {
+    "revision": "15e02766f3952a146e1495fd13a85f16",
+    "url": "/React-Weather-app/index.html"
+  }
+];
